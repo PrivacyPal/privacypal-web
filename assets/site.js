41faf1c016ab0ae4855e852baa30793cd08e162a
Jason Melo <jason.melo@gmail.com>
2026-05-17 01:09:53 -0400

    download modal

--- assets/site.js
+++ assets/site.js
@@ -159,7 +159,7 @@
             </div>
             <h4>macOS</h4>
             <p>Native builds for Apple Silicon and Intel Macs. Pick the architecture that matches your chip.</p>
-            <div class="pp-dl-pills"><span class="pp-dl-pill">.dmg installer</span><span class="pp-dl-pill">Universal binaries</span><span class="pp-dl-pill">macOS 13+</span></div>
+            <div class="pp-dl-pills"><span class="pp-dl-pill">.dmg installer</span><span class="pp-dl-pill">M1-M5+</span><span class="pp-dl-pill">macOS 13+</span></div>
             <a class="pp-dl-btn mac" href="${DOWNLOAD_MACOS_ARM64}" rel="noopener noreferrer">
               <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path d="M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09l.01-.01zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z"/></svg>
               Apple Silicon
