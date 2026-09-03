ad481ee7cd298c998018d7cbe3da839268e3c6b6
Jason Melo <jason.melo@gmail.com>
2026-09-02 22:23:30 -0400

    Meta tracking pixel

---
+++ assets/meta-pixel.js
@@ -0,0 +1,6 @@
+/* Meta Pixel — PrivacyPal (ID 1308351507879059)
+   Loaded from every page via <script src=".../assets/meta-pixel.js"> in <head>.
+   Add conversion events with fbq('track', 'Lead') etc. after this file loads. */
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1308351507879059');
+fbq('track', 'PageView');
